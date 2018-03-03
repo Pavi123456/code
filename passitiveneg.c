6a676dda09822b973a486616319618dc51f7a916
ashokkumarkalyani <37016363+ashokkumarkalyani@users.noreply.github.com>
2018-03-03 12:11:28 +0000

    Create passitiveneg.c

---
+++ passitiveneg.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num;
+printf("enter the no");
+scanf("%d",&num);
+if (num>=0)
+{
+if(num==0)
+{
+printf("you enter zero");
+else
+printf("the given number is pasitive");
+}
+printf("the given number isnegative");
+}
+}
