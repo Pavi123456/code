ef1e6185e8fde03c7b0770803d76b4844f7ff01a
ashokkumarkalyani <37016363+ashokkumarkalyani@users.noreply.github.com>
2018-03-03 12:23:55 +0000

    Create oddeven.c

---
+++ oddeven.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int s;
+printf("enter the integer value");
+scanf("%d",&s);
+{
+if(s%2==0)
+{
+printf("the given value is even");
+}
+else
+{
+printf("the given value is odd");
+}
+getch();
+}
+}
