8aa0bb5777bfb6f63c53f0897482f93c5d5e91d3
ashokkumarkalyani <37016363+ashokkumarkalyani@users.noreply.github.com>
2018-03-15 17:25:25 +0530

    Create arrayn.c

---
+++ arrayn.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void) 
+{
+int s,j,sum[100];
+printf("enter the number:");
+scanf("%d",&sum[9]);
+for(j=0;j<s;j++)
+{
+sum[j]=sum[j]+j;
+}
+printf("\n%d",sum);
+return 0;
+}
