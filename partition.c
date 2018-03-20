7afaee49ea3c5a8ae262cc7aded8546363e90503
ashokkumarkalyani <37016363+ashokkumarkalyani@users.noreply.github.com>
2018-03-20 17:53:51 +0530

    Create partition.c

---
+++ partition.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+unsigned long long s,x,x1;
+scanf("%d %d %d",&s,&x,&x1);
+if(n%2==0)
+{
+if(x==x1)
+{
+if((s/(x+x1))%2!=0)
+printf("s");
+{
+else
+}
+printf("no");
+}
+else
+ {
+   if((s/(x+x1))%2==0)
+   printf("s");
+}
+else
+{
+printf("no");
+}
+}
+else
+printf("no");
+return0;
+}
