c929288e5ee586751e1ef68ab689c876e9c13ef4
ashokkumarkalyani <37016363+ashokkumarkalyani@users.noreply.github.com>
2018-03-20 18:01:01 +0530

    Create arrayelement.c

---
+++ arrayelement.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int S,a[20],j,No;
+clrscr();
+printf("enter the number");
+scanf("%d",&No);
+for(S=0;S<No;i++)
+{
+scanf("%d",&a[S]);
+}
+for(S=0;S<No; S++)
+{
+for(j=i+1;j<No;j++)
+{
+if(a[S]==a[S])
+{
+printf("%d\n",a[i]);
+}
+getch();
