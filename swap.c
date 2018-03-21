0334fd4d094ab2633182f6bffb0c96f78fce3ca8
ashokkumarkalyani <37016363+ashokkumarkalyani@users.noreply.github.com>
2018-03-21 17:58:29 +0530

    Create swap.c

---
+++ swap.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char str[10];
+int k,tem,s;
+printf("enter string");
+scanf("&s",&str)
+n=strlen(str);
+if(s%2==0)
+{
+for(k=0;k<s;k+=2)
+{
+tem=str[k];
+string[k]=str[k+1];
+string[k+1]=tem;}
+puts(str);
+}
+else
+{
+for(k=0;k<n-1;k+=2)
+{
+tem=str[k];
+str[k]=str[k+1];
+str[k+1]=tem;
+}
+printf("the string is",&str)
+}getch();
+}
