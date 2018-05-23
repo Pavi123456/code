df39b150c79446d336f83ed62290f5e0c0206bfd
ashokkumarkalyani <37016363+ashokkumarkalyani@users.noreply.github.com>
2018-05-23 15:51:18 +0530

    Create first 5 multiples.c

---
+++ first 5 multiples.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+{
+    int n, i,N;
+    scanf("%d",&n);
+        for(i=1; i<=5; ++i)
+        {
+         N=n*i;
+        printf("%d\t",N);
+        }
+    return 0;
