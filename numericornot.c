899a6640c4f0c6078e8fa38cf8b0af22555a0e71
ashokkumarkalyani <37016363+ashokkumarkalyani@users.noreply.github.com>
2018-05-24 12:26:23 +0530

    Create numericornot.c

---
+++ numericornot.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(void) 
+{
+char string[10];
+int s,k;
+scanf("%s",string);
+n=strlen(string);
+for(k=0;k<s;k++)
+{
+if(string[k]>='0'&&string[k]<='9')
+{
+printf("\n YES");
+}
+else
+printf("\n NO");
+}
+return 0;
+}
