b41d97e8f8c1c8db97469286513c276d6ff9ae6e
ashokkumarkalyani <37016363+ashokkumarkalyani@users.noreply.github.com>
2018-05-25 14:49:19 +0530

    Create greater10.c

---
+++ greater10.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+ int main() {
+int a[10];
+int i;
+int great;
+for (i=0;i<10;i++) {
+scanf("%d",&a[i]);
+}
+great=a[0];
+for(i=0;i<10;i++) {
+if (a[i]>great)
+{
+great=a[i];
+}
+}
+printf("%d",great);
+return 0;
+ }
